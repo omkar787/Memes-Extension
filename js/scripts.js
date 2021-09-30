5ce818d47f2afc0abc4769501d04c452b611d546
Omkar Bhoir <omkarbhoir878@gmail.com>
2021-09-30 16:41:48 +0530

    Added some small functionalities

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
-const img = document.getElementById('img')
+// const img = document.getElementById('img')
 const next = document.getElementById('next')
 const copy = document.getElementById('copy')
-
+const image = document.getElementsByClassName('image')[0]
+const title = document.getElementsByClassName('title')[0]
+gimme()
 
 next.addEventListener('click',(e) => {
     img.setAttribute('alt','omkar')
+    gimme()
+})
+
+function gimme(){
+    title.innerHTML = "Loading..."
     fetch("https://meme-api.herokuapp.com/gimme")
         .then((response) =>{
             console.log(response);
@@ -12,8 +19,10 @@ next.addEventListener('click',(e) => {
         })
         .then((res) => {
             console.log(res);
-            img.setAttribute('src',res.url)
+            console.log(image);
+            title.innerHTML = `<h2>${res.title}</h2>`
+            image.innerHTML = `<img src="${res.url}" id="img" alt="Meme">`
+            // img.setAttribute('src',res.url)
         })
-})
-
+}
 
