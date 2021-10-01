a03f04a29870d627a730c60ff1e4aa20c548071e
Omkar Bhoir <omkarbhoir878@gmail.com>
2021-10-01 09:58:06 +0530

    working of copy button for png's

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,44 @@
-// const img = document.getElementById('img')
 const next = document.getElementById('next')
 const copy = document.getElementById('copy')
 const image = document.getElementsByClassName('image')[0]
 const title = document.getElementsByClassName('title')[0]
 gimme()
 
+
+// navigator.clipboard.writeText("Omkar")
+const status =  navigator.permissions.query({ name:'clipboard-write'})
+
 next.addEventListener('click',(e) => {
     img.setAttribute('alt','omkar')
     gimme()
 })
+
+copy.addEventListener('click',(e) => {
+    copyImage()
+})
+
+async function copyImage(){
+    const img = document.getElementById('img')
+    fetch(img.getAttribute('src'),{
+        origin:"*",
+        mode:'cors',
+        credentials:"omit"
+    })
+        .then(res => {
+            return res.blob()
+        })
+        .then(d => {
+            console.log(d);
+            navigator.clipboard.write([new ClipboardItem({[d.type]:d})])
+            console.log(d);
+        })
+    // imageURL.json().then(d => {console.log(d);})
+    // const blob = await imageURL.blob()
+    // console.log(blob);
+    // // await navigator.clipboard.write([new ClipboardItem({'image/png':blob})])
+    // console.log(blob)
+
+}
 
 function gimme(){
     title.innerHTML = "Loading..."
